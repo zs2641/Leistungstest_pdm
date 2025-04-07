b365bb3791f6b5e26e64b65d735b1903be433509
zs2641 <zs2641@mci4me.at>
2025-04-07 19:51:41 +0200

    test.py erstellt

---
+++ test.py
@@ -0,0 +1,15 @@
+from my_classes import Subject, Supervisor, Experiment
+
+if __name__ == "__main__":
+
+    # Erstellen eines Leistungstests
+    supervisor = Supervisor("FirstName", "LastName")
+    subject = Subject("FirstName", "LastName", "female", 30)
+    subject.estimate_max_hr()
+
+    experiment = Experiment("Leistungstest", "2021-01-01")
+    experiment.add_subject(subject)
+    experiment.add_supervisor(supervisor)
+
+
+    print(experiment)
